d082b806f4e60ade32e01e73daa6b5177b093bc7
Maria Gabriele <gabi2015maria2015@gmail.com>
2025-01-06 10:46:49 -0300

    refactor: ajustado

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,4 +1,4 @@
-const apiKey = "AIzaSyDSZWVo5zQ30v9N9p3Rdasq-2aHRp1Toh4";
+const apiKey = "";
 const header = {'Content-Type': 'application/json'};
 const urlGoogle =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDSZWVo5zQ30v9N9p3Rdasq-2aHRp1Toh4";
